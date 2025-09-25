dbe4f493291202e1739f3cea59b1dc27d274a23c
Anthony Alberto <2023260043@ub.edu.bz.com>
2025-09-25 18:21:56 +0000

    Added struct and insertUserprototype

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+struct User {
+	string username;
+	string password;
+	string role; //for authorizatio " author", editor etc.
+	User* next;
+
+	User(const string& u, const string& p, const string& r = "viewer") {
+		username = u;
+		password = p;
+		role = r;
+		next = nullptr;
+	}
+};
+
+bool insertUser(User*& head, const string& username, const string& password, const string& role = "viewer");
+
+
+
 int main() {
 
 	cout<< "Hello Worlrd! \n";
